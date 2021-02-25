c398433ac62089fbd61b64caedb936b44272b7a9
Lwasampijja Baker <bubakes2000@gmail.com>
2021-02-26 01:10:00 +0300

    Update gw_release_lambda_function.py

--- upwork-devs/Lwasampijja-Baker/gw_release_lambda_function.py
+++ upwork-devs/Lwasampijja-Baker/gw_release_lambda_function.py
@@ -3,7 +3,7 @@ import json
 from get_recent_releases import *
 
 def lambda_handler(event, context):
-    GetRelease().releases()
+    create_json()
     return {
         'body': json.dumps('JSON File created Successfully and uploaded to S3 bucket')
         }
